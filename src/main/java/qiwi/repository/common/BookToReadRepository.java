78c589cbb7e035c0ab2ad37b0cadd612581a7ffd
qiwi1104 <77515695+qiwi1104@users.noreply.github.com>
2021-07-26 18:31:28 +0400

    Delete src/main directory

--- src/main/java/qiwi/repository/common/BookToReadRepository.java
+++
@@ -1,7 +0,0 @@
-package qiwi.repository.common;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import qiwi.model.common.book.BookToRead;
-
-public interface BookToReadRepository<T extends BookToRead> extends JpaRepository<T, Integer> {
-}
--- src/main/java/qiwi/repository/common/FinishedBookRepository.java
+++
@@ -1,8 +0,0 @@
-package qiwi.repository.common;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import qiwi.model.common.book.FinishedBook;
-
-public interface FinishedBookRepository<T extends FinishedBook> extends JpaRepository<T, Integer> {
-
-}
