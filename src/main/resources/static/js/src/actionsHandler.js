175230b2ee8637dde5c0fbf674fae9781ceb8424
qiwi1104 <nikita.tim83@gmail.com>
2021-08-08 10:27:18 +0400

    small changes to html

--- src/main/resources/static/js/src/actionsHandler.js
+++ src/main/resources/static/js/src/actionsHandler.js
@@ -1,8 +1,17 @@
 function actionsHandler(id) {
+	let toHide = document.getElementsByClassName("hiddenPanel");
+	
+	for (let i = 0; i < toHide.length ; i++) {
+		toHide[i].hidden = true;
+	}
 
-            let toHide = document.getElementsByClassName("hiddenPanel");
-            for (let i = 0; i < toHide.length ; i++) {
-                toHide[i].hidden = true;
-            }
-            toHide[id].hidden = false;
+	toHide[id].hidden = false;
+
+	let panelElements = document.getElementsByClassName("panel")[0].children;
+
+	for (let i = 0; i < panelElements.length ; i++) {
+		panelElements[i].className = "underline";
+	}
+
+	document.getElementById(id).className = "underlineSelected";
 }
