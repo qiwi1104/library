2c6521196a6ff0a7f5445bdb043d90e9b6713885
qiwi1104 <nikita.tim83@gmail.com>
2021-08-29 08:45:16 +0400

    removed some code from repository package

--- src/main/java/qiwi/repository/spanish/FinishedBookSpanishRepository.java
+++ src/main/java/qiwi/repository/spanish/FinishedBookSpanishRepository.java
@@ -4,16 +4,6 @@ import org.springframework.stereotype.Service;
 import qiwi.model.spanish.FinishedBookSpanish;
 import qiwi.repository.common.FinishedBookRepository;
 
-import java.util.List;
-
 @Service
 public interface FinishedBookSpanishRepository extends FinishedBookRepository<FinishedBookSpanish> {
-    List<FinishedBookSpanish> findAllByOrderByIdAsc();
-    List<FinishedBookSpanish> findAllByOrderByIdDesc();
-
-    List<FinishedBookSpanish> findAllByOrderByStartAsc();
-    List<FinishedBookSpanish> findAllByOrderByStartDesc();
-
-    List<FinishedBookSpanish> findAllByOrderByEndAsc();
-    List<FinishedBookSpanish> findAllByOrderByEndDesc();
 }
