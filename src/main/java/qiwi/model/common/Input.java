733adcc77afbec27f8b3d6e631119062c9965bb7
qiwi1104 <nikita.tim83@gmail.com>
2021-08-30 07:49:23 +0400

    removed lombok

--- src/main/java/qiwi/model/common/Input.java
+++ src/main/java/qiwi/model/common/Input.java
@@ -1,10 +1,7 @@
 package qiwi.model.common;
 
-import lombok.Data;
-
 import java.sql.Date;
 
-@Data
 public class Input {
     private Integer id;
     private String author = "";
@@ -13,4 +10,60 @@ public class Input {
     private Date start = Date.valueOf("1970-1-1");
     private Date end = Date.valueOf("1970-1-1");
     private String description = "";
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getFound() {
+        return found;
+    }
+
+    public void setFound(Date found) {
+        this.found = found;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
