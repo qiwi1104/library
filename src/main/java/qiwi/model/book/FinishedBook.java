f707b84a96ec686a8aaa60d6fc837feddbffd116
qiwi1104 <nikita.tim83@gmail.com>
2022-02-08 00:27:55 +0400

    fixed hasDates condition

--- src/main/java/qiwi/model/book/FinishedBook.java
+++ src/main/java/qiwi/model/book/FinishedBook.java
@@ -52,7 +52,7 @@ public class FinishedBook extends Book implements Cloneable {
     }
 
     public boolean hasDates(AdditionalDates dates) {
-        return additionalDates.contains(dates) || dates.getStart().equals(start) || dates.getEnd().equals(end);
+        return additionalDates.contains(dates) || (dates.getStart().equals(start) && dates.getEnd().equals(end));
     }
 
     @Override
